6d8450fabd7c3df76bf0ec610a63e7bf60fc3c3d
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2025-04-21 13:15:52 -0300

    Add display board function

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include "hardware/clocks.h"
 
 #include "lib/led_matrix.h"
+#include "main.pio.h"
 
 /* Function prototypes*/
-void display_board(float board[5][5]);
+void display_board(char board[5][5], PIO pio, uint state_machine);
 
 
 int main()
@@ -17,18 +18,36 @@ int main()
         {'b', 'b', 'b', 'b', 'b'},
         {'e', 'b', 'e', 'b', 'e'},
         {'b', 'b', 'b', 'b', 'b'},
-        {'e', 'b', 'e', 'b', 'e'}
+        {'e', 'b', 'x', 'b', 'e'}
     };
+
+    bool x_turn = true; // Variable to track whose turn it is (X or O)
+    bool game_over = false; // Variable to track if the game is over
+    uint8_t cursor_position[2] = {2, 2}; // Variable to store the cursor position
+
+    
+
+    /* Clock configuration*/
+    if(set_sys_clock_khz(128000, false)){
+        printf("Configuration of system clock completed!\n");
+    }else{
+        printf("Configuration of system clock failed\n");
+        return -1;
+    }
+
+    /* PIO configuration*/
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &main_program);
+    uint state_machine = pio_claim_unused_sm(pio, true);
+    main_program_init(pio, state_machine, offset, GPIO_LED_MTX);
 
 
     stdio_init_all();
 
 
-    while(true) {
-        
-        display_board(board);
+    while(true) {      
+        display_board(board, pio, state_machine); // Display the board on the LED matrix
 
-        //TO DO: GAME2
         sleep_ms(1000); // Sleep for 1 second
     }
     return 0;
@@ -39,38 +58,32 @@ int main()
 
 
 
-void display_board(float board[5][5]) {
-    int i, j;
+void display_board(char board[5][5], PIO pio, uint state_machine) {
+    int i, j, step, start, end;
+    uint32_t led_value;
+
     // Display the board on the LED matrix
-    for (i = 4; i >=0; i--) {
+    for (i = 4; i >= 0; i--) {
+        step = (i & 1) ? 1 : -1; // Determine step direction based on row parity
+        start = (i & 1) ? 0 : 4; // Start index for the row
+        end = (i & 1) ? 5 : -1; // End index for the row
 
-        if (i & 1) { // Odd row
-            for (j = 0; j < 5; j++) {
-
-                switch (board[i][j]) {
-                    case 'x':
-                        matrix_rgb(0.1,0,0); // Blue
-                        break;
-                    case 'o':
-                        matrix_rgb(0,0.1,0); // Red
-                        break;
-                    case 'b':
-                        matrix_rgb(0.1, 0.1, 0.1); // White
-                        break;
-                    default:
-                        matrix_rgb(0,0,0); // Off
-                        break;
-                }
-                pio_sm_put_blocking(pio, state_machine, led_value);
-            
+        for (j = start; j != end; j += step) {
+            switch (board[i][j]) {
+                case 'x':
+                    led_value = matrix_rgb(0.1, 0, 0); // Blue
+                    break;
+                case 'o':
+                    led_value = matrix_rgb(0, 0.1, 0); // Red
+                    break;
+                case 'b':
+                    led_value = matrix_rgb(0.1, 0.1, 0.1); // White
+                    break;
+                default:
+                    led_value = matrix_rgb(0, 0, 0); // Off
+                    break;
             }
-        }else {
-            for (j = 4; j >= 0; j--) {
-            
-            }
+            pio_sm_put_blocking(pio, state_machine, led_value);
         }
-        printf("\n");
-
-            
     }
 }
